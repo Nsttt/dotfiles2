85d05684a5f9932a25a2230fd14bc74277f3f21d
Néstor <nestor@nstlopez.com>
2024-08-19 22:24:48 +0200

    feat: add nvim settings

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
+
+vim.filetype.add({
+    extension = {
+        templ = "templ"
+    }
+})
+vim.filetype.add({
+    extension = {
+        mdx = "mdx"
+    }
+})
